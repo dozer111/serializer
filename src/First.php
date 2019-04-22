61168b0708e80c8965ad9ccfa32dfe800226a35a
dozer111 <alkhonko@gmail.com>
2019-04-22 22:18:12 +0300

    first commit

---
+++ src/First.php
@@ -0,0 +1,29 @@
+<?php
+namespace dozer111\serializer;
+
+
+
+
+class First
+{
+    public function __construct()
+    {
+        echo "Well done".PHP_EOL;
+        echo __DIR__;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
